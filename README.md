911cb810db04d08556bd169f06b44142941cdb10
gabarbudo <gabrielle.barbudo@obf.ateneo.edu>
2018-10-19 21:37:26 +0800

    Updated code

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Problem Solving Agent
+## Problem Solving Agent
+# The Claw
+### by Tomy Callanta and Gab Barbudo
+
+## Agent and Environment Specifications
+- Environment has two (2) sides: left (L) and right (R)
+- Only one (1) side can contain an object
+- Goal-based agent may be in L or R
+- Agent can perceive if a side contains an object
+- Agent can only hold or release an object, and move to the right or to the left, and upward or downward
+
+## Successor Functions
+- Down (D): Crane moves downward
+- Up (U): Crane moves upward
+- Right (Ri): Crane moves to the right
+- Left (L): Crane moves to the left
+- Hold (H): Crane Claw picks up object
+- Release (Re): Crane Claw lets go of object
+
+## How to start the program
+1. Input the desired initial state.
+2. Input the desired goal state.
+3. Choose the search strategy to be used. Input 'bfs' for Breadth-First Search or 'iddfs' for Iterative Deepening Depth-First Search.
+4. The output will be the series of actions and states the agent passes through to arrive at the goal state.
--- code/base.py
+++ code/base.py
@@ -1,12 +1,46 @@
 from collections import deque
 
+def do_successor(node):
+	current_state = node.state
+	successors =[]
+	if current_state[0] == True:
+		successors.append(('D', [False] + current_state[1:]))
+		OBJECT_LEFT = current_state[3]
+		
+		if current_state[1] == True:
+			if current_state[4] == True:
+				OBJECT_LEFT = False
+			successors.append(('Ri', current_state[0:1] + [False] + current_state[2:3] + [OBJECT_LEFT] + current_state[4:]))
+		else:
+			if current_state[4] == True:
+				OBJECT_LEFT = True
+			successors.append(('L', current_state[0:1] + [True] + current_state[2:3] + [OBJECT_LEFT] + current_state[4:]))
+	else:
+		successors.append(('U', [True] + current_state[1:]))
+
+	if current_state[2] == True:
+		if current_state[0] == False and current_state[1] == current_state[3]:
+			successors.append(('H', current_state[0:2] + [False] + current_state[3:4] + [True]))
+		else:
+			successors.append(('H', current_state[0:2] + [False] + current_state[3:]))
+	else:
+		if current_state[4] == True:
+			successors.append(('Re', current_state[0:2] + [True] + current_state[3:4] + [False]))
+		else:
+			successors.append(('Re', current_state[0:2] + [True] + current_state[3:]))
+
+	return successors
+
+def step_cost_fn(state, action, result):
+	return 1
+
 class Problem:
     #must input initial state, a successor function, and a goal
     def __init__(self, init_state, successor_fn, step_cost, goal):
         """
         Problem contains methods and values to describe a problem that can be solve a general problem
 
-        init_stat = node describes initial state where search for the solution starts
+        init_state = node describes initial state where search for the solution starts
         successor_fn = function to create new node states based on the specifications of the problem
         step_cost = function to compute for doing an action to move from one node to another 
         goal = node that describes the end goal where search stops
@@ -22,15 +56,31 @@ class Problem:
     def remove_front(self, fringe, search):
         if search == "bfs":
             return fringe.popleft()
-        elif search == "dfs":
+        elif search == "iddfs":
             return fringe.pop()
 
     def get_path(self, node):
         path = [node]
-        while node.parent is not None:
-            path.append(node.parent)
+        curr = node.parent
+        print("--------------------")
+        print(node.action)
+        print(node.state)
+        while curr is not None:
+            path.append(curr)
+            print(curr.action)
+            print(curr.state)
+            curr = curr.parent
 
         return path
+
+    def init_state(state):
+    	label_crane = "Crane: "
+    	label_object = "Object: "
+    	if state == 0:
+    		CLAW_DOWN = False
+    		CRANE_RIGHT = False
+    		CLAW_CLOSED = False
+    		OBJECT_RIGHT = False
 
     def expand(self, node):
         """
@@ -48,42 +98,74 @@ class Problem:
     def tree_solve(self, search="bfs"):
         if search == "bfs":
             fringe = deque([]) #fringe is a queue
-        elif search == "dfs":
+        elif search == "iddfs":
             fringe = [] #fringe is a regular list but can be used as a stack
 
         fringe.append(self.init_state)
 
+        counter = 0
         while True:
-            if not fringe: #checks if fringe is empty
-                return None #return null if no solution
-            
-            node = self.remove_front(fringe, search)
+        	counter = counter + 1
+        	if search == "iddfs":
+        		while True:
+        			print('Depth: ' + str(counter))
+        			if not fringe:
+        				break
 
-            if self.goal_test(node):
-                return self.get_path(node)
+        			node = self.remove_front(fringe, search)
+        			depth = node.path_cost
+        			print(node.action)
+        			print(node.state)
 
-            new_nodes = self.expand(node)
+        			if self.goal_test(node):
+        				return self.get_path(node)
 
-            for n in new_nodes:
-                fringe.append(n)
+        			if node.path_cost + 1 <= counter:
+        				new_nodes = self.expand(node)
+        				for n in new_nodes:
+        					fringe.append(n)
+
+        		fringe = []
+        		fringe.append(self.init_state)
+
+        	elif search == "bfs":
+        		if not fringe: #checks if fringe is empty
+        			return None #return null if no solution
+
+        		node = self.remove_front(fringe, search)
+        		depth = node.path_cost
+        		print('Depth: ' + str(depth))
+        		print(node.action)
+        		print(node.state)
+
+        		if self.goal_test(node):
+        			return self.get_path(node)
+
+        		new_nodes = self.expand(node)
+
+        		for n in new_nodes:
+        			fringe.append(n)
 
 class ProblemState:
-    def __new__(cls, state, prev_node = None, action=None, step_cost=0):
-        """
-        Only inistantiates node if previous node and action are either both null or both not null
+    # def __new__(cls, state, prev_node = None, action=None, step_cost=0):
+    #     """
+    #     Only inistantiates node if previous node and action are either both null or both not null
 
-        prev_node = previous / parent node of node about to be intialized. Can be null if initial state, but action must also be null.
-        action = action done to reach node about to be initialized from previous node
-        state = ordered list of values that describe the state of the node
-        """
-        if (action is not None and prev_node is not None) or (action is None and prev_node is None):
-            return super().__init__(prev_node, action, state, step_cost)
-        else: #don't initialize
-            return None
+    #     prev_node = previous / parent node of node about to be intialized. Can be null if initial state, but action must also be null.
+    #     action = action done to reach node about to be initialized from previous node
+    #     state = ordered list of values that describe the state of the node
+    #     """
+    #     print(state)
+    #     if (action is not None and prev_node is not None) or (action is None and prev_node is None):
+    #         return super().__init__(prev_node, action, state, step_cost)
+    #         print('hi')
+    #     else: #don't initialize
+    #         return None
 
     def __init__(self, state, prev_node=None, action=None, step_cost=0):
         self.parent = prev_node
         self.state = state
+        self.action = action
 
         if prev_node is not None:
             self.depth = prev_node.depth + 1
@@ -98,3 +180,26 @@ class ProblemState:
             return self.state == other.state
         else:
             return False
+
+# 
+print('The Claw')
+print('Input initial state: ')
+init_state = input()
+init_array = []
+for i in init_state.split(' '):
+	init_array.append(i == 'True')
+
+init_state = ProblemState(init_array)
+
+print('Input goal state: ')
+goal_state = input()
+goal_array = []
+for i in goal_state.split(' '):
+	goal_array.append(i == 'True')
+goal_state = ProblemState(goal_array)
+
+print('Strategy to be used (bfs or iddfs): ')
+strategy = input()
+
+problem = Problem(init_state, do_successor, step_cost_fn, goal_state)
+problem.tree_solve(strategy)
